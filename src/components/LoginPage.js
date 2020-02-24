40fe994c168b42cbc7befed4f3195aec05225bfc
Mohammed AlMuhanna <almuhannamh@outlook.com>
2020-02-24 21:27:56 +0300

    update the api, improve auth

--- src/components/LoginPage.js
+++ src/components/LoginPage.js
@@ -1,26 +1,33 @@
 import React from "react";
 import Authentication from '../Authentication'
+import validateUser from '../api/validateUser'
 export default class LoginPage extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            isInvalid: false,
+            userId: 0,
+            paswd: ''
+        }
+    }
+
     validateLogin = (e) => {
         e.preventDefault();
         const userId = e.target.elements.userId.value
         const paswd = e.target.elements.paswd.value
+        console.log(validateUser);
 
         if ((userId == 11) && (paswd == '123')) {
             this.props.history.push("/NewMRO")
-            // this.setState({
-            //     isLoggedIn: !this.state.isLoggedIn
-            // });
             Authentication.isAuthenticated = !Authentication.isAuthenticated
-            // console.log(Authentication.isAuthenticated = !Authentication.isAuthenticated);
-
         } else {
             this.setState({
-                isInvalid: !this.state.isInvalid
+                isInvalid: true
             });
         }
     }
     render() {
+        let errMsg = this.state.isInvalid ? 'wrong information' : ''
         return (
             <>
                 <h1>Login Page</h1>
@@ -28,6 +35,7 @@ export default class LoginPage extends React.Component {
                 <form method="post" onSubmit={(e) => this.validateLogin(e)}>
                     <fieldset>
                         <legend><strong>Login</strong></legend>
+                        {errMsg}
                         <br />
                         <label htmlFor="userId">User ID: </label>
                         <input type="text" name="userId" />
