d88d999b34334750927bc986f67c9cf45b13c909
Mohammed AlMuhanna <almuhannamh@outlook.com>
2020-02-26 16:05:17 +0300

    Updates

---
+++ precache-manifest.16e6c1525b09196726877b88b7910668.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d4a3126cef59290c3efcbb4bdee57ce4",
+    "url": "/Warehouse-Solution/index.html"
+  },
+  {
+    "revision": "8a0cabe57b73c4e20439",
+    "url": "/Warehouse-Solution/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "05e95fd5c631486c90d9",
+    "url": "/Warehouse-Solution/static/js/2.21658b13.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/Warehouse-Solution/static/js/2.21658b13.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8a0cabe57b73c4e20439",
+    "url": "/Warehouse-Solution/static/js/main.a9063a93.chunk.js"
+  },
+  {
+    "revision": "cdae39d327a2c10899c3",
+    "url": "/Warehouse-Solution/static/js/runtime-main.e9af03ca.js"
+  }
+]);
